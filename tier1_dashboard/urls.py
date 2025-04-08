b30b34e7a53bc7d5ab9292876a301c7c17ca7a4b
aghajani <aghajani.ali@gmail.com>
2025-04-08 08:43:31 +0330

    bug fix

--- tier1_dashboard/urls.py
+++ tier1_dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import Tier1AlertListView, Tier1AlertDetailView
+from .views import Tier1AlertListView # Removed Tier1AlertDetailView
 from .api.views import Tier1AlertDataAPIView
 
 app_name = 'tier1_dashboard'
@@ -10,10 +10,6 @@ urlpatterns = [
         Tier1AlertListView.as_view(),
         name='tier1-unacked-alerts'
     ),
-    path(
-        'alerts/<str:fingerprint>/',
-        Tier1AlertDetailView.as_view(),
-        name='tier1-alert-detail'
-    ),
+    # Removed unused Tier 1 specific alert detail URL
     path('api/alerts/', Tier1AlertDataAPIView.as_view(), name='api_alert_data'),
 ]
