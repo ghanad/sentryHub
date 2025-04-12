29596aa8408fb873f059635007c53335e2ecf790
aghajani.ali <aghajani.ali@gmail.com>
2025-04-12 04:45:35 +0000

    update urls

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views as core_views
+from . import views
 
 app_name = 'core'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('about/', core_views.AboutView.as_view(), name='about'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('login/', views.LoginView.as_view(), name='login'),
 ] 
