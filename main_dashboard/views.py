43ea2ff0e37bc5d295cb09f9a01d9bf9524d0870
aghajani.ali <aghajani.ali@gmail.com>
2025-04-12 05:08:08 +0000

    update views

--- main_dashboard/views.py
+++ main_dashboard/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from datetime import timedelta
 
-import json
+import json, logging
 from alerts.models import AlertGroup
 
 logger = logging.getLogger(__name__)
