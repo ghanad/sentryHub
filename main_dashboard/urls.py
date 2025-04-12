1d975a34b10b35f5208eeb765752d8b48e83e8a4
aghajani.ali <aghajani.ali@gmail.com>
2025-04-12 05:06:48 +0000

    update main_dashboard urls

--- main_dashboard/urls.py
+++ main_dashboard/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 
-app_name = 'dashboard'
+from .views import DashboardView
+from documentations.api import views as docs_api_views
 
+app_name = 'main_dashboard'
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
 ]
