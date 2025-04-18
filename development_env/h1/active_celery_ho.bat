8f8523586e063aa181b9a2b70a9029687a255b8f
aghajani <am.ag65@gmail.com>
2025-04-18 10:40:58 +0330

    add env files

---
+++ development_env/h1/active_celery_ho.bat
@@ -0,0 +1,34 @@
+@echo off
+rem This batch file activates a Python virtual environment and runs a Celery worker
+
+rem Set the paths based on your provided information
+set VENV_PATH=C:\codes\python_codes\prometheus_alerts\venv
+set PROJECT_PATH=C:\codes\python_codes\prometheus_alerts\sentryHub
+
+rem Activate the virtual environment
+echo Activating virtual environment...
+call "%VENV_PATH%\Scripts\activate.bat"
+
+rem Check if virtual environment was activated successfully
+if %ERRORLEVEL% neq 0 (
+    echo Failed to activate virtual environment!
+    pause
+    exit /b %ERRORLEVEL%
+)
+
+rem Navigate to the project directory
+echo Navigating to project directory...
+cd /d "%PROJECT_PATH%"
+
+rem Run the Celery worker
+echo Running Celery worker...
+celery -A sentryHub worker --loglevel=info
+
+rem Keep the window open if there's an error
+if %ERRORLEVEL% neq 0 (
+    echo An error occurred while running the Celery worker.
+    pause
+    exit /b %ERRORLEVEL%
+)
+
+echo Process completed successfully.
