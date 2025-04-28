fc9a4b34b64bda130670e49f08e2d3205a9fbb81
aghajani <aghajani.ali@gmail.com>
2025-04-28 13:50:22 +0330

    Revert "update settings"
    This reverts commit 83535b236f14664fd595a656bc7c366fc5cdc1b9.

--- sentryHub/settings.py
+++ sentryHub/settings.py
@@ -258,12 +258,9 @@ CELERY_TASK_ROUTES = {
 # Removed redundant serializer settings
 CELERY_TIMEZONE = TIME_ZONE
 
-SITE_URL = "https://pmt.tsetmc.com"
+
 JIRA_CONFIG = {
     'server_url': 'https://jira.tsetmc.com',
-    'username': 'username',
-    'password': 'password',
-    'allowed_project_keys': ['SAM'],
     'open_status_categories': ['To Do', 'In Progress'],
     'closed_status_categories': ['Done'],
 
