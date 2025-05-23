aaf2b42f0ca73ec2d1f66fcccc1056eda77cf0bb
Aghajani <aghajani.ali@gmail.com>
2025-05-23 13:21:07 +0330

    feat: Import test modules in __init__.py to enhance test discoverability for Django's test runner

--- core/tests/__init__.py
+++ core/tests/__init__.py
@@ -1,1 +1,6 @@
 # This file makes the 'tests' directory a Python package.
+
+# Import test modules to make them discoverable by Django's test runner
+from . import test_context_processors
+from . import test_middleware
+from . import test_views
